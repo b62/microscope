d732d6517fbaeaaded4d51b929ed7d540856f6f2
Maksym Kundos <mkundos@cisco.com>
2015-09-14 21:13:50 -0700

    helpers added

--- microscope/client/views/posts/post_item.js
+++ microscope/client/views/posts/post_item.js
@@ -1,3 +1,11 @@
 /**
  * Created by mkundos on 9/14/2015.
  */
+
+Template.postItem.helpers({
+    domain: function(){
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
